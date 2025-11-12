602954dd531f9c25b687d85bbacc68d1db1f1aba
Nur Fahmi <hello@rfahmi.com>
2025-11-12 12:44:30 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-// app.js (shim Passenger)
+process.env.UNDICI_NO_WASM = '1';        // paksa Undici tanpa WASM
+process.env.NODE_OPTIONS = process.env.NODE_OPTIONS || '--max-old-space-size=256';
+
 process.chdir(__dirname);
 require('dotenv').config();
 require('./src/index.js');
